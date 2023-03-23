54593a7d3af39d526e13f88f6edcfc47f929f2d1
crescentpartha <crescentpartha406@gmail.com>
2023-03-23 12:31:26 +0600

    get all menu data from database

--- index.js
+++ index.js
@@ -13,12 +13,32 @@ app.use(express.json());
 // connection setup with database with secure password on environment variable
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxegwwi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('TikFood database connected');
-    // perform actions on the collection object
-    client.close();
-});
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('TikFood database connected');
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+// Create dynamic data and send to the database
+async function run() {
+    try {
+        await client.connect();
+        const menuCollection = client.db('tikfoodDB').collection('menu');
+
+        // get all menu data (json format) from database
+        app.get('/menu', async (req, res) => {
+            const query = {};
+            const cursor = menuCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    }
+    finally {
+        // await client.close(); // commented, if I want to keep connection active;
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running TikFood Server');
